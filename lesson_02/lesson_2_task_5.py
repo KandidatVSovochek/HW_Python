d243f348e35738c07cdf3a77ab885c37cc11fa76
KandidatVSovochek <kruteva2000@mail.ru>
2026-01-28 22:58:51 +0300

    Fifth_commit

--- lesson_02/lesson_2_task_5.py
+++ lesson_02/lesson_2_task_5.py
@@ -1,11 +1,11 @@
 def month_to_season(n):
-    if n <=2 and n == 12:
+    if n <=2 or n == 12:
         print ("Зима")
     elif 3 <= n <= 5:
         print ("Весна")
     elif 6 <= n <= 8:
-        print ("Лето")
-    else: 9 <= n <= 11
-    print("Осень")
+        print ("Лето")        
+    elif 9 <= n <= 11:
+     print("Осень")
 n = int(input("Введите порядковый номер месяца: "))
 month_to_season(n)
